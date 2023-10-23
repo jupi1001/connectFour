e03b6e6984cc9151bf48be0c658ee37603935a93
Laurids Hieden <realjupi1001@gmail.com>
2023-10-23 16:49:32 +0200

    Working a bit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const board = document.getElementById("board");
   const resetButton = document.getElementById("reset");
+  const getBestMoveButton = document.getElementById("bestMove");
   const cells = [];
   const boardState = Array.from({ length: 6 }, () => Array(7).fill(0));
   let currentPlayer = 1;
@@ -11,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let j = 0; j < 7; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.dataset.row = i; // Set the data-row attribute
+      cell.dataset.col = j; // Set the data-col attribute
       cell.row = i;
       cell.col = j;
       cells[i][j] = cell;
@@ -18,11 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function dropPiece(col) {
+  function dropPiece(board, col, simulated) {
     for (let i = 5; i >= 0; i--) {
-      if (boardState[i][col] === 0) {
-        boardState[i][col] = currentPlayer;
-        cells[i][col].classList.add(`player-${currentPlayer}`);
+      if (board[i][col] === 0) {
+        board[i][col] = currentPlayer;
+        if (!simulated) {
+          cells[i][col].classList.add(`player-${currentPlayer}`);
+        }
         return i;
       }
     }
@@ -62,8 +67,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function handleClick(event) {
+    removeRecommendation();
     const col = event.target.col;
-    const row = dropPiece(col);
+    const row = dropPiece(boardState, col, false);
     if (row !== -1) {
       if (checkWinner(row, col)) {
         alert(`Player ${currentPlayer} wins!`);
@@ -80,13 +86,87 @@ document.addEventListener("DOMContentLoaded", function () {
     currentPlayer = 1;
   }
 
+  function getBestMove() {
+    removeRecommendation();
+    let bestMove = null;
+    let bestEval = -Infinity;
+
+    for (let col = 0; col < 7; col++) {
+      if (boardState[0][col] === 0) {
+        const newBoard = boardState.map((row) => row.slice());
+        const eval = minimax(newBoard, 5, -Infinity, Infinity, false);
+        if (eval > bestEval) {
+          bestEval = eval;
+          bestMove = col;
+        }
+      }
+    }
+    console.log(bestMove);
+    highlightRecommendation(bestMove);
+    return bestMove;
+  }
+
+  function minimax(board, depth, alpha, beta, maximizingPlayer) {
+    if (depth === 0 || isGameOver(board)) {
+      return evaluateBoard(board);
+    }
+
+    if (maximizingPlayer) {
+      let maxEval = -Infinity;
+      for (let col = 0; col < 7; col++) {
+        if (board[0][col] === 0) {
+          const row = dropPiece(board, col, true); // Pass the board to dropPiece
+          const eval = minimax(board, depth - 1, alpha, beta, false);
+          maxEval = Math.max(maxEval, eval);
+          alpha = Math.max(alpha, eval);
+          if (beta <= alpha) break; // Alpha-beta pruning
+        }
+      }
+      return maxEval;
+    } else {
+      let minEval = Infinity;
+      for (let col = 0; col < 7; col++) {
+        if (board[0][col] === 0) {
+          const row = dropPiece(board, col, true); // Pass the board to dropPiece
+          const eval = minimax(board, depth - 1, alpha, beta, true);
+          minEval = Math.min(minEval, eval);
+          beta = Math.min(beta, eval);
+          if (beta <= alpha) break; // Alpha-beta pruning
+        }
+      }
+      return minEval;
+    }
+  }
+
+  function isGameOver(board) {
+    return checkWinner(0, 0) || board.flat().every((cell) => cell !== 0);
+  }
+
+  function evaluateBoard(board) {
+    if (checkWinner(0, 0)) {
+      return -1; // Player 2 wins
+    } else if (checkWinner(0, 1)) {
+      return 1; // Player 1 wins
+    } else {
+      return 0; // Draw
+    }
+  }
+
+  function highlightRecommendation(col) {
+    for (let i = 0; i < 6; i++) {
+      const cell = document.querySelector(`[data-col='${col}'][data-row='${i}']`);
+      if (cell) {
+        cell.classList.add("recommendation");
+      }
+    }
+  }
+
+  function removeRecommendation() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => cell.classList.remove("recommendation"));
+  }
+
   board.addEventListener("click", handleClick);
   resetButton.addEventListener("click", resetBoard);
-
-  // Export necessary functions for later use in minimax algorithm
-  window.ConnectFour = {
-    dropPiece,
-    checkWinner,
-    currentPlayer,
-  };
+  getBestMoveButton.addEventListener("click", getBestMove);
 });
